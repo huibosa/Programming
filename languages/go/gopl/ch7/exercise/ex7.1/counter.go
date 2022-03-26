6b775419ecbb92bdc79a69abb321791857292853
huibosa_arch <huibosa@gmail.com>
2022-03-26 09:27:53 +0800

    update

--- languages/go/gopl/ch7/exercise/ex7.1/counter.go
+++ languages/go/gopl/ch7/exercise/ex7.1/counter.go
@@ -1,9 +1,20 @@
-package main
+package counter
 
 import (
 	"bufio"
 	"bytes"
 )
+
+type LineCounter int
+
+func (c *LineCounter) Write(p []byte) (int, error) {
+	buf := bytes.NewBuffer(p)
+	s := bufio.NewScanner(buf)
+	for s.Scan() {
+		*c++
+	}
+	return int(*c), nil
+}
 
 type WordCounter int
 
