001341fd2a30ce601902096f5b0aa9e3bf94563b
huibosa_arch <huibosa@gmail.com>
2022-06-10 21:59:12 +0800

    update

--- course/15-213/homework-problems/src/8.24.c
+++ course/15-213/homework-problems/src/8.24.c
@@ -1,6 +1,7 @@
 #include <asm-generic/errno-base.h>
 #include <csapp.h>
 #include <errno.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,7 +9,8 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define N 10
+#define N 2
+#define LEN 100
 
 int main(void) {
   pid_t pid;
@@ -16,7 +18,8 @@ int main(void) {
 
   for (i = 0; i < N; ++i) {
     if ((pid = Fork()) == 0) {
-      exit(100 + i);
+      char* ptr = NULL;
+      *ptr = 'h';
     }
   }
 
@@ -24,6 +27,10 @@ int main(void) {
     if (WIFEXITED(status)) {
       printf("child %d terminated normally with exit status=%d\n", pid,
              WEXITSTATUS(status));
+    } else if (WIFSIGNALED(status)) {
+      char buf[LEN];
+      sprintf(buf, "child %d terminated by signal %d", pid, WTERMSIG(status));
+      psignal(WTERMSIG(status), buf);
     } else {
       printf("child %d terminated abnormally\n", pid);
     }
